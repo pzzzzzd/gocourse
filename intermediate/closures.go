2827cb643e326405d9e80c5651d864ce95fe2ad3
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2025-11-25 22:25:21 +0300

    closures

---
+++ intermediate/closures.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// sequence := adder()
+
+	// fmt.Println(sequence())
+	// fmt.Println(sequence())
+	// fmt.Println(sequence())
+	// fmt.Println(sequence())
+
+	// sequence2 := adder()
+	// fmt.Println(sequence2())
+
+	subtracter := func() func(int) int {
+
+		countdown := 99
+		return func(x int) int {
+			countdown -= x
+			return countdown
+		}
+
+	}()
+
+	// Using the closure subtracter
+	fmt.Println(subtracter(1))
+	fmt.Println(subtracter(2))
+	fmt.Println(subtracter(3))
+	fmt.Println(subtracter(4))
+	fmt.Println(subtracter(5))
+	fmt.Println(subtracter(6))
+
+}
+
+func adder() func() int {
+	i := 0
+	fmt.Println("Previous value of i:", i)
+	return func() int {
+		i++
+		fmt.Println("Added 1 to i")
+		return i
+	}
+}
