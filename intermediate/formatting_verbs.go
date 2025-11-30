ec906bbc71e51abc8e676c23c4e93ecec8c5c81f
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2025-11-30 22:23:21 +0300

    formatting_verbs

---
+++ intermediate/formatting_verbs.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	// i := 121_435.5_23
+	// string := "Hello World!"
+
+	// fmt.Printf("%v\n", i)
+	// fmt.Printf("%#v\n", i)
+	// fmt.Printf("%T\n", i)
+	// fmt.Printf("%v%%\n", i)
+
+	// fmt.Printf("%v\n", string)
+	// fmt.Printf("%#v\n", string)
+	// fmt.Printf("%T\n", string)
+
+	// int := 27
+
+	// fmt.Printf("%b\n", int)
+	// fmt.Printf("%d\n", int)
+	// fmt.Printf("%+d\n", int)
+	// fmt.Printf("%o\n", int)
+	// fmt.Printf("%O\n", int)
+	// fmt.Printf("%x\n", int)
+	// fmt.Printf("%X\n", int)
+	// fmt.Printf("%#x\n", int)
+	// fmt.Printf("%4d\n", int)
+	// fmt.Printf("%-4d\n", int)
+	// fmt.Printf("%04d\n", int)
+
+	// txt := "World"
+
+	// fmt.Printf("%s\n", txt)
+	// fmt.Printf("%q\n", txt)
+	// fmt.Printf("%100s\n", txt) // space
+	// fmt.Printf("%-8s\n", txt)
+	// fmt.Printf("%x\n", txt)
+	// fmt.Printf("% x\n", txt)
+
+	// t := true
+	// f := false
+
+	// fmt.Printf("%t\n", t)
+	// fmt.Printf("%v\n", t)
+	// fmt.Printf("%t\n", f)
+
+	flt := 1.98323
+
+	fmt.Printf("%e\n", flt)
+	fmt.Printf("%f\n", flt)
+	fmt.Printf("%.2f\n", flt)
+	fmt.Printf("%100.2f\n", flt) // 100 space .2 00
+	fmt.Printf("%g\n", flt)
+
+}
