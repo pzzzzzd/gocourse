9dd7d3c1a64b246d9fc0a0cb5b209728df8189a2
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-01-20 19:43:15 +0300

    closing_channels

---
+++ advanced/closing_channels.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+func producer(ch chan<- int) {
+	for i := range 5 {
+		ch <- i
+	}
+	close(ch)
+}
+
+func filter(in <-chan int, out chan<- int) {
+	for val := range in {
+		if val%2 == 0 {
+			out <- val
+		}
+	}
+	close(out)
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go producer(ch1)
+	go filter(ch1, ch2)
+	for val := range ch2 {
+		fmt.Println(val)
+	}
+}
+
+// func main() {
+// 	ch := make(chan int)
+// 	go func() {
+// 		close(ch)
+// 		// close(ch)
+// 	}()
+// 	time.Sleep(time.Second)
+// }
+
+//------- Range over closed channel -------
+// func main() {
+// 	ch := make(chan int)
+// 	go func() {
+// 		for i := range 5 {
+// 			ch <- i
+// 		}
+// 		close(ch)
+// 	}()
+// 	for val := range ch {
+// 		fmt.Println("Received:", val)
+// 	}
+// }
+
+//------- Received from a closed channel -------
+// func main() {
+// 	ch := make(chan int)
+// 	close(ch)
+// 	val, ok := <-ch
+// 	if !ok {
+// 		fmt.Println("Channel is closed")
+// 	} else {
+// 		fmt.Println(val)
+// 	}
+// }
+
+//------- Simple closing channel -------
+// func main() {
+// 	ch := make(chan int)
+// 	go func() {
+// 		for i := range 5 {
+// 			ch <- i
+// 		}
+// 		close(ch)
+// 	}()
+// 	for val := range ch {
+// 		fmt.Println(val)
+// 	}
+// }
