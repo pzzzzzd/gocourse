a0367599cfe29df8aa9fb9d211036d8ece5a7b89
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-02-05 18:50:56 +0300

    rate_limiting_token_bucket_algo

---
+++ advanced/rate_limiting_token_bucket_algo.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type RateLimiter struct {
+	mu        sync.Mutex
+	count     int
+	limit     int
+	window    time.Duration
+	resetTime time.Time
+}
+
+func NewRateLimiter(limit int, window time.Duration) *RateLimiter {
+	return &RateLimiter{
+		limit:  limit,
+		window: window,
+	}
+}
+
+func (rl *RateLimiter) Allow() bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+
+	now := time.Now()
+	if now.After(rl.resetTime) {
+		rl.resetTime = now.Add(rl.window)
+		rl.count = 0
+	}
+
+	if rl.count < rl.limit {
+		rl.count++
+		return true
+	}
+	return false
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	rateLimiter := NewRateLimiter(3, 1*time.Second)
+
+	for range 10 {
+		wg.Add(1)
+		if rateLimiter.Allow() {
+			fmt.Println("Request Allowed")
+		} else {
+			fmt.Println("Request Denied")
+		}
+		wg.Done()
+		// time.Sleep(200 * time.Millisecond)
+	}
+	wg.Wait()
+
+}
