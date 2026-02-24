04e4af5b4696fa1c9ad82a528fc2c39a4b18428f
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-02-24 21:02:39 +0300

    signals (#41)
    * signals

---
+++ advanced/signals.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	pid := os.Getpid()
+	fmt.Println("Process ID:", pid)
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGHUP)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println("Received signal:", sig)
+		done <- true
+	}()
+
+	go func() {
+
+		for {
+			select {
+			case <-done:
+				fmt.Println("Stopping work due to signal.")
+				// os.Exit(0)
+				return
+			default:
+				fmt.Println("Working...")
+				time.Sleep(time.Second)
+			}
+		}
+		// sig := <-sigs
+		// for sig := range sigs {
+		// 	switch sig {
+		// 	case syscall.SIGINT:
+		// 		fmt.Println("Received SIGINT (Interrupt)")
+		// 	// case syscall.SIGTERM:
+		// 	// 	fmt.Println("Received SIGTERM (Terminate)")
+		// 	case syscall.SIGHUP:
+		// 		fmt.Println("Received SIGHUP (Hangup)")
+		// 	case syscall.SIGUSR1:
+		// 		fmt.Println("Received SIGNUSR1 (User defined Signal 1)")
+		// 		fmt.Println("User define function is executed")
+		// 	}
+		// 	// fmt.Println("Graceful exit.")
+		// 	// os.Exit(0)
+		// }
+	}()
+
+	// fmt.Println("Working...")
+	for {
+		time.Sleep(time.Second)
+	}
+}
