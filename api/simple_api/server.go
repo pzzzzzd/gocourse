94486d89f060637774e6cb56c117953154ca9d2e
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-05-14 13:42:38 +0300

    14_05_2026 (#48)

--- api/simple_api/server.go
+++ api/simple_api/server.go
@@ -12,10 +12,12 @@ import (
 func main() {
 
 	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
 		fmt.Fprintf(w, "Handling incoming orders")
 	})
 
 	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		logRequestDetails(r)
 		fmt.Fprintf(w, "Handling users")
 	})
 
@@ -47,4 +49,31 @@ func main() {
 		log.Fatalln("Could not start server", err)
 	}
 
+}
+
+func logRequestDetails(r *http.Request) {
+	httpVersion := r.Proto
+	fmt.Println("Received request with HTTP version:", httpVersion)
+
+	if r.TLS != nil {
+		tlsVersion := getTLSVersionName(r.TLS.Version)
+		fmt.Println("Received request with TLS version:", tlsVersion)
+	} else {
+		fmt.Println("Received request without TLS")
+	}
+}
+
+func getTLSVersionName(version uint16) string {
+	switch version {
+	case tls.VersionTLS10:
+		return "TLS 1.0"
+	case tls.VersionTLS11:
+		return "TLS 1.1"
+	case tls.VersionTLS12:
+		return "TLS 1.2"
+	case tls.VersionTLS13:
+		return "TLS 1.3"
+	default:
+		return "Unknown TLS version"
+	}
 }
