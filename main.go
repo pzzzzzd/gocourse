e9b4436f0843b36ba495ef5f1b3822157c97f37e
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-05-19 22:42:38 +0300

    19_05_2026

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+)
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func main() {
+	user := User{Name: "Egor", Email: "exp@exp.com"}
+	fmt.Println(user)
+	jsonData, err := json.Marshal(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(jsonData))
+
+	var user1 User
+	err = json.Unmarshal(jsonData, &user1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(user1)
+
+	jsonData1 := `{"name": "NN", "email": "NN@exp.com"}`
+	reader := strings.NewReader(jsonData1)
+	decoder := json.NewDecoder(reader)
+
+	var user2 User
+	err = decoder.Decode(&user2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(user2)
+
+	var buf bytes.Buffer
+	encoder := json.NewEncoder(&buf)
+
+	err = encoder.Encode(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(buf.String())
+}
