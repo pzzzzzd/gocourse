4905c7a55ca6c49e8b9a5318a7a4c0567eaaf7c9
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-07-08 18:14:57 +0300

    08_07_2026 (#64)

---
+++ rest_api_go/internal/repository/sqlconnect/slqconfig.go
@@ -0,0 +1,21 @@
+package sqlconnect
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectDb(dbname string) (*sql.DB, error) {
+	fmt.Println("Trying to connect MariaDB")
+
+	connectionString := "root:5206@tcp(127.0.0.1:3306)/" + dbname
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		// panic(err)
+		return nil, err
+	}
+	fmt.Println("Connected to MariaDB")
+	return db, nil
+}
