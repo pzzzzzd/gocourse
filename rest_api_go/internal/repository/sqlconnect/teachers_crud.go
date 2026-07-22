5fca3a4e51cfbffbed94bbd00d05951a46b9ea5e
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-07-22 15:36:12 +0300

    22_07_2026 (#72)

---
+++ rest_api_go/internal/repository/sqlconnect/teachers_crud.go
@@ -0,0 +1,394 @@
+package sqlconnect
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"restapi/internal/models"
+	"strconv"
+	"strings"
+)
+
+func isValidSortOrder(order string) bool {
+	return order == "asc" || order == "desc"
+}
+
+func isValidSortField(field string) bool {
+	validFields := map[string]bool{
+		"first_name": true,
+		"last_name":  true,
+		"email":      true,
+		"class":      true,
+		"subject":    true,
+		"id":         true,
+	}
+	return validFields[field]
+}
+
+func addSorting(r *http.Request, query string) string {
+	sortParams := r.URL.Query()["sortby"]
+	if len(sortParams) > 0 {
+		query += " ORDER BY"
+		for i, param := range sortParams {
+			parts := strings.Split(param, ":")
+			if len(parts) != 2 {
+				continue
+			}
+			field, order := parts[0], parts[1]
+			if !isValidSortField(field) || !isValidSortOrder(order) {
+				continue
+			}
+			if i > 0 {
+				query += ","
+			}
+			query += " " + field + " " + order
+		}
+	}
+	return query
+}
+
+func addFilters(r *http.Request, query string, args []interface{}) (string, []interface{}) {
+	params := map[string]string{
+		"first_name": "first_name",
+		"last_name":  "last_name",
+		"email":      "email",
+		"class":      "class",
+		"subject":    "subject",
+	}
+
+	for param, dbField := range params {
+		value := r.URL.Query().Get(param)
+		if value != "" {
+			query += " AND " + dbField + " =?"
+			args = append(args, value)
+		}
+	}
+	return query, args
+}
+
+func GetTeachersDbHandler(teachers []models.Teacher, r *http.Request) ([]models.Teacher, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	query := "SELECT id, first_name, last_name, email, class, subject FROM teachers WHERE 1=1"
+	var args []interface{}
+
+	query, args = addFilters(r, query, args)
+
+	query = addSorting(r, query)
+
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var teacher models.Teacher
+		err = rows.Scan(&teacher.ID, &teacher.FirstName, &teacher.LastName, &teacher.Email, &teacher.Class, &teacher.Subject)
+		if err != nil {
+			return nil, err
+		}
+		teachers = append(teachers, teacher)
+	}
+	return teachers, nil
+}
+
+func GetTeacherByID(id int) (models.Teacher, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return models.Teacher{}, err
+	}
+
+	defer db.Close()
+
+	var teacher models.Teacher
+	err = db.QueryRow("SELECT id, first_name, last_name, email, class, subject FROM teachers WHERE id = ?", id).Scan(&teacher.ID, &teacher.FirstName, &teacher.LastName,
+		&teacher.Email, &teacher.Class, &teacher.Subject)
+	if err == sql.ErrNoRows {
+		return models.Teacher{}, err
+	} else if err != nil {
+		fmt.Println(err)
+		return models.Teacher{}, err
+	}
+	return teacher, nil
+}
+
+func AddTeachersDbHandler(newTeachers []models.Teacher) ([]models.Teacher, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO teachers (first_name, last_name, email, class, subject) VALUES (?,?,?,?,?)")
+	if err != nil {
+		return nil, err
+	}
+
+	addedTeachers := make([]models.Teacher, len(newTeachers))
+	for i, newTeacher := range newTeachers {
+		res, err := stmt.Exec(newTeacher.FirstName, newTeacher.LastName, newTeacher.Email, newTeacher.Class, newTeacher.Subject)
+		if err != nil {
+			return nil, err
+		}
+		lastID, err := res.LastInsertId()
+		if err != nil {
+			return nil, err
+		}
+		newTeacher.ID = int(lastID)
+		addedTeachers[i] = newTeacher
+	}
+	return addedTeachers, nil
+}
+
+func UpdateTeacher(id int, updatedTeacher models.Teacher) (models.Teacher, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return models.Teacher{}, err
+	}
+	defer db.Close()
+
+	var existingTeacher models.Teacher
+	err = db.QueryRow("SELECT id, first_name, last_name, email, class, subject FROM teachers WHERE id = ?", id).Scan(&existingTeacher.ID, &existingTeacher.FirstName, &existingTeacher.LastName,
+		&existingTeacher.Email, &existingTeacher.Class, &existingTeacher.Subject)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return models.Teacher{}, err
+		}
+		return models.Teacher{}, err
+	}
+
+	updatedTeacher.ID = existingTeacher.ID
+	_, err = db.Exec("UPDATE teachers SET first_name = ?, last_name = ?, email = ?, class = ?, subject = ? WHERE id = ?", updatedTeacher.FirstName, updatedTeacher.LastName, updatedTeacher.Email,
+		updatedTeacher.Class, updatedTeacher.Subject, updatedTeacher.ID)
+	if err == sql.ErrNoRows {
+		return models.Teacher{}, err
+	}
+	return updatedTeacher, nil
+}
+
+func PatchTeachers(updates []map[string]interface{}) error {
+	db, err := ConnectDb()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	for _, update := range updates {
+		idStr, ok := update["id"].(string)
+		if !ok {
+			tx.Rollback()
+			return err
+		}
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		var teacherFromDb models.Teacher
+		err = db.QueryRow("SELECT id, first_name, last_name, email, class, subject FROM teachers WHERE id = ?", id).Scan(&teacherFromDb.ID, &teacherFromDb.FirstName,
+			&teacherFromDb.LastName, &teacherFromDb.Email, &teacherFromDb.Class, &teacherFromDb.Subject)
+
+		if err != nil {
+			log.Println("ID:", id)
+			log.Printf("Type: %T", id)
+			log.Println(err)
+			tx.Rollback()
+			if err == sql.ErrNoRows {
+				return err
+			}
+			return err
+		}
+
+		teacherVal := reflect.ValueOf(&teacherFromDb).Elem()
+		teacherType := teacherVal.Type()
+		for k, v := range update {
+			if k == "id" {
+				continue
+			}
+			for i := 0; i < teacherVal.NumField(); i++ {
+
+				field := teacherType.Field(i)
+				if field.Tag.Get("json") == k+",omitempty" {
+					fieldVal := teacherVal.Field(i)
+					if fieldVal.CanSet() {
+						val := reflect.ValueOf(v)
+						if val.Type().ConvertibleTo(fieldVal.Type()) {
+							fieldVal.Set(val.Convert(fieldVal.Type()))
+						} else {
+							tx.Rollback()
+							log.Printf("Cant convert %v to %v\n", val.Type(), fieldVal.Type())
+							return err
+						}
+
+					}
+					break
+				}
+			}
+		}
+		_, err = tx.Exec("UPDATE teachers SET first_name = ?, last_name = ?, email = ?, class = ?, subject = ? WHERE id = ?", teacherFromDb.FirstName, teacherFromDb.LastName, teacherFromDb.Email,
+			teacherFromDb.Class, teacherFromDb.Subject, teacherFromDb.ID)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PatchOneTeacher(id int, updates map[string]interface{}) (models.Teacher, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		log.Println(err)
+		return models.Teacher{}, err
+	}
+	defer db.Close()
+
+	var existingTeacher models.Teacher
+	err = db.QueryRow("SELECT id, first_name, last_name, email, class, subject FROM teachers WHERE id = ?", id).Scan(&existingTeacher.ID, &existingTeacher.FirstName, &existingTeacher.LastName,
+		&existingTeacher.Email, &existingTeacher.Class, &existingTeacher.Subject)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println(err)
+			return models.Teacher{}, err
+		}
+		log.Println(err)
+
+		return models.Teacher{}, err
+	}
+
+	teacherVal := reflect.ValueOf(&existingTeacher).Elem()
+	teacherType := teacherVal.Type()
+
+	for k, v := range updates {
+		for i := 0; i < teacherVal.NumField(); i++ {
+			field := teacherType.Field(i)
+			if field.Tag.Get("json") == k+",omitempty" {
+				if teacherVal.Field(i).CanSet() {
+					fieldVal := teacherVal.Field(i)
+					fmt.Println("fieldVal", fieldVal)
+					fmt.Println("teacherVal.Field(i).Type()", teacherVal.Field(i).Type())
+					fmt.Println("reflect.ValueOf(v)", reflect.ValueOf(v))
+					fieldVal.Set(reflect.ValueOf(v).Convert(teacherVal.Field(i).Type()))
+				}
+			}
+		}
+	}
+
+	_, err = db.Exec("UPDATE teachers SET first_name = ?, last_name = ?, email = ?, class = ?, subject = ? WHERE id = ?", existingTeacher.FirstName, existingTeacher.LastName, existingTeacher.Email,
+		existingTeacher.Class, existingTeacher.Subject, existingTeacher.ID)
+	if err == sql.ErrNoRows {
+		log.Println(err)
+
+		return models.Teacher{}, err
+	}
+	return existingTeacher, nil
+}
+
+func DeleteOneTeacher(id int) error {
+	db, err := ConnectDb()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer db.Close()
+
+	result, err := db.Exec("DELETE FROM teachers WHERE id = ?", id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return err
+	}
+	return nil
+}
+
+func DeleteTeachers(ids []int) ([]int, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	stmt, err := tx.Prepare("DELETE FROM teachers WHERE id = ?")
+	if err != nil {
+		log.Println(err)
+		tx.Rollback()
+		return nil, err
+	}
+	defer stmt.Close()
+
+	deletedIds := []int{}
+	for _, id := range ids {
+		result, err := stmt.Exec(id)
+		if err != nil {
+			tx.Rollback()
+			log.Println(err)
+
+			return nil, err
+		}
+
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			tx.Rollback()
+
+			return nil, err
+		}
+		if rowsAffected > 0 {
+			deletedIds = append(deletedIds, id)
+		}
+		if rowsAffected < 1 {
+			tx.Rollback()
+
+			return nil, err
+		}
+
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Println(err)
+
+		return nil, err
+	}
+
+	if len(deletedIds) < 1 {
+
+		return nil, err
+	}
+	return deletedIds, nil
+}
