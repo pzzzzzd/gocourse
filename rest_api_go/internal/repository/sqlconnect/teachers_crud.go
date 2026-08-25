dbb4d7b1a50cd2affdee67de160dccab0e266617
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-08-25 13:12:13 +0300

    25_08_2026

--- rest_api_go/internal/repository/sqlconnect/teachers_crud.go
+++ rest_api_go/internal/repository/sqlconnect/teachers_crud.go
@@ -351,3 +351,49 @@ func DeleteTeachers(ids []int) ([]int, error) {
 	}
 	return deletedIds, nil
 }
+
+func GetStudentsByTeacherIdFromDb(teacherId string, students []models.Student) ([]models.Student, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "error retrieving data")
+	}
+	defer db.Close()
+
+	query := `SELECT id, first_name, last_name, email, class FROM students WHERE class = (SELECT class from teachers WHERE id = ?)`
+	rows, err := db.Query(query, teacherId)
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "error retrieving data")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var student models.Student
+		err := rows.Scan(&student.ID, &student.FirstName, &student.LastName, &student.Email, &student.Class)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, "error retrieving data")
+		}
+		students = append(students, student)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "error retrieving data")
+	}
+	return students, nil
+}
+
+func GetStudentCountByTeacherIdFromDb(teacherId string) (int, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		log.Println(err)
+		return 0, utils.ErrorHandler(err, "error retrieving data")
+	}
+	defer db.Close()
+
+	query := `SELECT COUNT(*) FROM students WHERE class = (SELECT class FROM teachers WHERE id = ?)`
+	var studentCount int
+	err = db.QueryRow(query, teacherId).Scan(&studentCount)
+	if err != nil {
+		return 0, utils.ErrorHandler(err, "error retrieving data")
+	}
+	return studentCount, nil
+}
