8fc1d8ad0a8a53c795b40aeb98f3a56a016ef8e7
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-09-15 00:27:22 +0300

    14_09_2026 (#77)

---
+++ rest_api_go/internal/repository/sqlconnect/exec_crud.go
@@ -0,0 +1,308 @@
+package sqlconnect
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"reflect"
+	"restapi/internal/models"
+	"restapi/pkg/utils"
+	"strconv"
+)
+
+func GetExecsDbHandler(execs []models.Exec, r *http.Request) ([]models.Exec, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "error retrieving data")
+	}
+
+	defer db.Close()
+
+	query := "SELECT id, first_name, last_name, email, username, user_created_at, inactive_status, role FROM execs WHERE 1=1"
+	var args []interface{}
+
+	query, args = utils.AddFilters(r, query, args)
+
+	query = utils.AddSorting(r, query)
+
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		fmt.Println(err)
+		return nil, utils.ErrorHandler(err, "error retrieving data")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var exec models.Exec
+		err := rows.Scan(&exec.ID, &exec.FirstName, &exec.LastName, &exec.Email, &exec.Username, &exec.UserCreatedAt, &exec.InactiveStatus, &exec.Role)
+		if err != nil {
+			return nil, err
+		}
+		execs = append(execs, exec)
+	}
+	return execs, nil
+}
+
+func GetExecByID(id int) (models.Exec, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return models.Exec{}, utils.ErrorHandler(err, "error retrieving data")
+	}
+
+	defer db.Close()
+
+	var exec models.Exec
+	err = db.QueryRow("SELECT id, first_name, last_name, email, username, inactive_status, role FROM execs WHERE id = ?", id).Scan(&exec.ID, &exec.FirstName, &exec.LastName,
+		&exec.Email, &exec.Username, &exec.InactiveStatus, &exec.Role)
+	if err == sql.ErrNoRows {
+		return models.Exec{}, utils.ErrorHandler(err, "error retrieving data")
+	} else if err != nil {
+		return models.Exec{}, utils.ErrorHandler(err, "error retrieving data")
+	}
+	return exec, nil
+}
+
+func AddExecsDbHandler(newExecs []models.Exec) ([]models.Exec, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "error adding data 1")
+	}
+
+	defer db.Close()
+
+	// stmt, err := db.Prepare("INSERT INTO execs (first_name, last_name, email) VALUES (?,?,?,?)")
+	stmt, err := db.Prepare(utils.GenerateInsertQuery("execs", models.Exec{}))
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "error adding data 2")
+	}
+
+	addedExecs := make([]models.Exec, len(newExecs))
+	for i, newExec := range newExecs {
+		// res, err := stmt.Exec(newExec.FirstName, newExec.LastName, newExec.Email, newExec.Class, newExec.Subject)
+		values := utils.GetStructValues(newExec)
+		res, err := stmt.Exec(values...)
+		if err != nil {
+			return nil, utils.ErrorHandler(err, "error adding data 3")
+		}
+		lastID, err := res.LastInsertId()
+		if err != nil {
+			return nil, utils.ErrorHandler(err, "error adding data 4")
+		}
+		newExec.ID = int(lastID)
+		addedExecs[i] = newExec
+	}
+	return addedExecs, nil
+}
+
+func PatchExecs(updates []map[string]interface{}) error {
+	db, err := ConnectDb()
+	if err != nil {
+		return utils.ErrorHandler(err, "Invalid ID")
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		return utils.ErrorHandler(err, "Invalid ID")
+	}
+
+	for _, update := range updates {
+		idStr, ok := update["id"].(string)
+		if !ok {
+			tx.Rollback()
+			return utils.ErrorHandler(err, "Invalid ID")
+		}
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			tx.Rollback()
+			return utils.ErrorHandler(err, "Invalid ID")
+		}
+
+		var execFromDb models.Exec
+		err = db.QueryRow("SELECT id, first_name, last_name, email, username FROM execs WHERE id = ?", id).Scan(&execFromDb.ID, &execFromDb.FirstName,
+			&execFromDb.LastName, &execFromDb.Email, &execFromDb.Username)
+
+		if err != nil {
+			tx.Rollback()
+			if err == sql.ErrNoRows {
+				return utils.ErrorHandler(err, "exec not found")
+			}
+			return utils.ErrorHandler(err, "Invalid ID")
+		}
+
+		execVal := reflect.ValueOf(&execFromDb).Elem()
+		execType := execVal.Type()
+		for k, v := range update {
+			if k == "id" {
+				continue
+			}
+			for i := 0; i < execVal.NumField(); i++ {
+
+				field := execType.Field(i)
+				if field.Tag.Get("json") == k+",omitempty" {
+					fieldVal := execVal.Field(i)
+					if fieldVal.CanSet() {
+						val := reflect.ValueOf(v)
+						if val.Type().ConvertibleTo(fieldVal.Type()) {
+							fieldVal.Set(val.Convert(fieldVal.Type()))
+						} else {
+							tx.Rollback()
+							log.Printf("Cant convert %v to %v\n", val.Type(), fieldVal.Type())
+							return utils.ErrorHandler(err, "Invalid ID")
+						}
+
+					}
+					break
+				}
+			}
+		}
+		_, err = tx.Exec("UPDATE execs SET first_name = ?, last_name = ?, email = ?, username = ? WHERE id = ?", execFromDb.FirstName, execFromDb.LastName, execFromDb.Email, execFromDb.ID,
+			execFromDb.Username)
+		if err != nil {
+			tx.Rollback()
+			return utils.ErrorHandler(err, "error updating data")
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return utils.ErrorHandler(err, "error updating data")
+	}
+	return nil
+}
+
+func PatchOneExec(id int, updates map[string]interface{}) (models.Exec, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		log.Println(err)
+		return models.Exec{}, utils.ErrorHandler(err, "error updating data")
+	}
+	defer db.Close()
+
+	var existingExec models.Exec
+	err = db.QueryRow("SELECT id, first_name, last_name, email, username FROM execs WHERE id = ?", id).Scan(&existingExec.ID, &existingExec.FirstName, &existingExec.LastName,
+		&existingExec.Email, &existingExec.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Println(err)
+			return models.Exec{}, utils.ErrorHandler(err, "exec not found")
+		}
+		return models.Exec{}, utils.ErrorHandler(err, "error updating data")
+	}
+
+	execVal := reflect.ValueOf(&existingExec).Elem()
+	execType := execVal.Type()
+
+	for k, v := range updates {
+		for i := 0; i < execVal.NumField(); i++ {
+			field := execType.Field(i)
+			if field.Tag.Get("json") == k+",omitempty" {
+				if execVal.Field(i).CanSet() {
+					fieldVal := execVal.Field(i)
+					fmt.Println("fieldVal", fieldVal)
+					fmt.Println("execVal.Field(i).Type()", execVal.Field(i).Type())
+					fmt.Println("reflect.ValueOf(v)", reflect.ValueOf(v))
+					fieldVal.Set(reflect.ValueOf(v).Convert(execVal.Field(i).Type()))
+				}
+			}
+		}
+	}
+
+	_, err = db.Exec("UPDATE execs SET first_name = ?, last_name = ?, email = ?, username = ? WHERE id = ?", existingExec.FirstName, existingExec.LastName, existingExec.Email,
+		existingExec.ID, existingExec.Username)
+	if err == sql.ErrNoRows {
+		log.Println(err)
+
+		return models.Exec{}, utils.ErrorHandler(err, "error updating data")
+	}
+	return existingExec, nil
+}
+
+func DeleteOneExec(id int) error {
+	db, err := ConnectDb()
+	if err != nil {
+		log.Println(err)
+		return utils.ErrorHandler(err, "error deleting exec")
+	}
+	defer db.Close()
+
+	result, err := db.Exec("DELETE FROM execs WHERE id = ?", id)
+	if err != nil {
+		return utils.ErrorHandler(err, "error deleting exec")
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return utils.ErrorHandler(err, "error deleting exec")
+	}
+
+	if rowsAffected == 0 {
+		return utils.ErrorHandler(err, "exec not found")
+	}
+	return nil
+}
+
+func DeleteExecs(ids []int) ([]int, error) {
+	db, err := ConnectDb()
+	if err != nil {
+		log.Println(err)
+		return nil, utils.ErrorHandler(err, "error deleting exec")
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Println(err)
+		return nil, utils.ErrorHandler(err, "error deleting exec")
+	}
+
+	stmt, err := tx.Prepare("DELETE FROM execs WHERE id = ?")
+	if err != nil {
+		log.Println(err)
+		tx.Rollback()
+		return nil, utils.ErrorHandler(err, "error deleting exec")
+	}
+	defer stmt.Close()
+
+	deletedIds := []int{}
+	for _, id := range ids {
+		result, err := stmt.Exec(id)
+		if err != nil {
+			tx.Rollback()
+			log.Println(err)
+
+			return nil, utils.ErrorHandler(err, "error deleting exec")
+		}
+
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			tx.Rollback()
+
+			return nil, utils.ErrorHandler(err, "error deleting exec")
+		}
+		if rowsAffected > 0 {
+			deletedIds = append(deletedIds, id)
+		}
+		if rowsAffected < 1 {
+			tx.Rollback()
+
+			return nil, utils.ErrorHandler(err, fmt.Sprintf("ID %d not found", id))
+		}
+
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Println(err)
+
+		return nil, utils.ErrorHandler(err, "error deleting exec")
+	}
+
+	if len(deletedIds) < 1 {
+
+		return nil, utils.ErrorHandler(err, "IDs do not found")
+	}
+	return deletedIds, nil
+}
