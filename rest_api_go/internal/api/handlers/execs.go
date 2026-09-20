2af8c33695431dfe5ccf49570abdf758513a92bd
pzzzzzd <240294525+pzzzzzd@users.noreply.github.com>
2026-09-20 21:48:56 +0300

    20_09_2026

--- rest_api_go/internal/api/handlers/execs.go
+++ rest_api_go/internal/api/handlers/execs.go
@@ -1,11 +1,7 @@
 package handlers
 
 import (
-	"crypto/subtle"
-	"database/sql"
-	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"io"
 	"log"
 	"net/http"
@@ -13,10 +9,7 @@ import (
 	"restapi/internal/repository/sqlconnect"
 	"restapi/pkg/utils"
 	"strconv"
-	"strings"
 	"time"
-
-	"golang.org/x/crypto/argon2"
 )
 
 func GetExecsHandler(w http.ResponseWriter, r *http.Request) {
@@ -219,22 +212,9 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	db, err := sqlconnect.ConnectDb()
+	user, err := sqlconnect.GetUserByUsername(req.Username)
 	if err != nil {
-		utils.ErrorHandler(err, "error updating data")
-		http.Error(w, "error connecting to database", http.StatusBadRequest)
-		return
-	}
-	defer db.Close()
-
-	user := &models.Exec{}
-	err = db.QueryRow("SELECT id, first_name, last_name, email, username, password, inactive_status, role FROM execs WHERE username = ?", req.Username).Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Username, &user.Password, &user.InactiveStatus, &user.Role) // errr declared and not used
-	if err != nil {
-		if err == sql.ErrNoRows {
-			utils.ErrorHandler(err, "user not found")
-			http.Error(w, "user not found", http.StatusBadRequest)
-		}
-		http.Error(w, "database query error", http.StatusBadRequest)
+		http.Error(w, "Invalid username or password", http.StatusBadRequest)
 		return
 	}
 
@@ -243,43 +223,9 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	parts := strings.Split(user.Password, ".")
-	if len(parts) != 2 {
-		utils.ErrorHandler(errors.New("invalid encoded hash format"), "invalid encoded hash format")
-		http.Error(w, "invalid encoded hash format", http.StatusForbidden)
-		return
-	}
-
-	saltBase64 := parts[0]
-	hashedPasswordBase64 := parts[1]
-
-	salt, err := base64.StdEncoding.DecodeString(saltBase64)
+	err = utils.VerifyPassword(req.Password, user.Password)
 	if err != nil {
-		utils.ErrorHandler(err, "failed to decode the salt")
-		http.Error(w, "failed to decode the salt", http.StatusForbidden)
-		return
-	}
-
-	hashedPassword, err := base64.StdEncoding.DecodeString(hashedPasswordBase64)
-	if err != nil {
-		utils.ErrorHandler(err, "failed to decode the hashed password")
-		http.Error(w, "failed to decode the hashed password", http.StatusForbidden)
-		return
-	}
-
-	hash := argon2.IDKey([]byte(req.Password), salt, 1, 64*1024, 4, 32)
-
-	if len(hash) != len(hashedPassword) {
-		utils.ErrorHandler(errors.New("incorrect password"), "incorrect password")
-		http.Error(w, "incorrect password", http.StatusForbidden)
-		return
-	}
-
-	if subtle.ConstantTimeCompare(hash, hashedPassword) == 1 {
-
-	} else {
-		utils.ErrorHandler(errors.New("incorrect password"), "incorrect password")
-		http.Error(w, "incorrect password", http.StatusForbidden)
+		http.Error(w, err.Error(), http.StatusForbidden)
 		return
 	}
 
@@ -296,6 +242,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		HttpOnly: true,
 		Secure:   true,
 		Expires:  time.Now().Add(24 * time.Hour),
+		SameSite: http.SameSiteStrictMode,
 	})
 
 	http.SetCookie(w, &http.Cookie{
@@ -305,6 +252,7 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		HttpOnly: true,
 		Secure:   true,
 		Expires:  time.Now().Add(24 * time.Hour),
+		SameSite: http.SameSiteStrictMode,
 	})
 
 	w.Header().Set("Content-Type", "application/json")
@@ -315,4 +263,19 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(response)
 
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "Bearer",
+		Value:    "",
+		Path:     "/",
+		HttpOnly: true,
+		Secure:   true,
+		Expires:  time.Unix(0, 0),
+		SameSite: http.SameSiteStrictMode,
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "Logged out successfully"}`))
 }
